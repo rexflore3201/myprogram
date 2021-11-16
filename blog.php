b3e36408f86469138f9151cf11c838ca745e2853
unknown <gregianflorenosos21@gmail.com>
2021-11-16 12:58:30 +0800

    i made changes

---
+++ blog.php
@@ -0,0 +1,108 @@
+<?php
+    include_once 'header.php';
+?>
+
+<section>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Greg Ian S. Florenosos">
+    <meta name="generator" content="Greg 0.84.0">
+    <title>Checkout example · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="/img/img/feed-image-1.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Associate in Computer Technology</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/img/img/feed-image-2.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Bachelor of Science in Information Technology</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/img/img/feed-image-3.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Bachelor of Science in Education</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/img/img/feed-image-4.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Bachelor of Science in Accountancy</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/img/img/feed-image-5.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Bachelor of Science in Business Administrator</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/img/img/advertisement.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Bachelor of Science in Computer Science</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      <script src="form-validation.js"></script>
+  </body>
+</html>
+
+        </section>
+<?php
+    include_once 'footer.php';
+?>
--- profile.php
+++ profile.php
@@ -1,34 +1,9 @@
 <?php
     include_once 'header.php';
 ?>
-<section id="about-us" class="p-5">
-                    <div class="text-center col-md">
-                        <h2 class="text-center mb-4">University of <span class="text-warning">Banica</span></h2>
-                        <span class="text-center mb-5">Banica Talamban Cebu City</span>
-                        <ul class="list-group list-group-flush lead">
-                            <li class="list-group-item">
-                                <span class="fw-bold">Official Study Load</span></li>
-                                <span class="fw-bold">Student's Copy</span></li><br><br>
-                                <p class="text-md-end">OSL No.:</p><input class="text-md-end" type="number" name="ON" placeholder="OSL No.">
-                                <p class="text-md-start">Student Number:</p><input type="number" name="SN" placeholder="Student Number">
-                                <p class="text-md-start">Student Status:</p><input type="number" name="SS" placeholder="Student Status">
-                            <li class="list-group-item">
-                                <span class="fw-bold text-right">Student Number:</span> (555) 234-4567
-                            </li>
-                            <li class="list-group-item">
-                                <span class="fw-bold">Enrollment Phone:</span> 0977-283-1753
-                            </li>
-                            <li class="list-group-item">
-                                <span class="fw-bold">Official Enrollment Email:</span> gregianflorenosos21@gmail.com
-                            </li>
-                            <li class="list-group-item">
-                                <span class="fw-bold">Alternate Enrollment Email:</span> gregianflorenosos26@gmail.com
-                            </li>
-                        </ul>  
-                    </div>
-        </section>
-        <section>
-        <!doctype html>
+
+<section>
+<!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -70,60 +45,11 @@
 <div class="container">
   <main>
     <div class="py-5 text-center">
-      <h2>Checkout form</h2>
-      <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+      <h2>University of Banica</h2>
+      <p class="lead">Fill up the form to continue your application.</p>
     </div>
-
-    <div class="row g-5">
-      <div class="col-md-5 col-lg-4 order-md-last">
-        <h4 class="d-flex justify-content-between align-items-center mb-3">
-          <span class="text-primary">Your cart</span>
-          <span class="badge bg-primary rounded-pill">3</span>
-        </h4>
-        <ul class="list-group mb-3">
-          <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Product name</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$12</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Second product</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$8</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-              <h6 class="my-0">Third item</h6>
-              <small class="text-muted">Brief description</small>
-            </div>
-            <span class="text-muted">$5</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between bg-light">
-            <div class="text-success">
-              <h6 class="my-0">Promo code</h6>
-              <small>EXAMPLECODE</small>
-            </div>
-            <span class="text-success">−$5</span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between">
-            <span>Total (USD)</span>
-            <strong>$20</strong>
-          </li>
-        </ul>
-
-        <form class="card p-2">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Promo code">
-            <button type="submit" class="btn btn-secondary">Redeem</button>
-          </div>
-        </form>
-      </div>
       <div class="col-md-7 col-lg-8">
-        <h4 class="mb-3">Billing address</h4>
+        <h4 class="mb-3">Student Details</h4>
         <form class="needs-validation" novalidate>
           <div class="row g-3">
             <div class="col-sm-6">
@@ -142,16 +68,47 @@
               </div>
             </div>
 
-            <div class="col-12">
-              <label for="username" class="form-label">Username</label>
-              <div class="input-group has-validation">
-                <span class="input-group-text">@</span>
-                <input type="text" class="form-control" id="username" placeholder="Username" required>
+            <div class="col-md-5">
+              <label for="country" class="form-label">Course</label>
+              <select class="form-select" id="country" required>
+                <option value="">Choose...</option>
+                <option>Associate in Computer Technology</option>
+                <option>Bachelor of Science in Information Technology</option>
+                <option>Bachelor of Science in Education</option>
+                <option>Bachelor of Science in Accountancy</option>
+                <option>Bachelor of Science in Business Administration</option>
+                <option>Bachelor of Science in Computer Science</option>
+              </select>
               <div class="invalid-feedback">
-                  Your username is required.
-                </div>
+                Please select a Course.
               </div>
             </div>
+
+            <div class="col-md-3">
+              <label for="zip" class="form-label">Age</label>
+              <input type="text" class="form-control" id="age" placeholder="" required>
+              <div class="invalid-feedback">
+                Age is required.
+              </div>
+            </div>
+
+          <div class="col-md-3">
+              <label for="zip" class="form-label">Mobile Number</label>
+              <input type="text" class="form-control" id="mobile" placeholder="" required>
+              <div class="invalid-feedback">
+                Mobile Number is required.
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+              <label for="zip" class="form-label">Birtdate</label>
+              <input type="text" class="form-control" id="birthdate" placeholder="" required>
+              <div class="invalid-feedback">
+                Birthdate is required.
+              </div>
+            </div>
+          </div>
 
             <div class="col-12">
               <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
@@ -169,105 +126,22 @@
               </div>
             </div>
 
-            <div class="col-12">
-              <label for="address2" class="form-label">Address 2 <span class="text-muted">(Optional)</span></label>
-              <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
-            </div>
-
-            <div class="col-md-5">
-              <label for="country" class="form-label">Country</label>
-              <select class="form-select" id="country" required>
-                <option value="">Choose...</option>
-                <option>United States</option>
-              </select>
-              <div class="invalid-feedback">
-                Please select a valid country.
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <label for="state" class="form-label">State</label>
-              <select class="form-select" id="state" required>
-                <option value="">Choose...</option>
-                <option>California</option>
-              </select>
-              <div class="invalid-feedback">
-                Please provide a valid state.
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="zip" class="form-label">Zip</label>
-              <input type="text" class="form-control" id="zip" placeholder="" required>
-              <div class="invalid-feedback">
-                Zip code required.
-              </div>
-            </div>
-          </div>
-
           <hr class="my-4">
 
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="same-address">
-            <label class="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
-          </div>
-
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="save-info">
-            <label class="form-check-label" for="save-info">Save this information for next time</label>
-          </div>
-
-          <hr class="my-4">
-
-          <h4 class="mb-3">Payment</h4>
+          <h4 class="mb-3">Student Type</h4>
 
           <div class="my-3">
             <div class="form-check">
               <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required>
-              <label class="form-check-label" for="credit">Credit card</label>
+              <label class="form-check-label" for="credit">New Student</label>
             </div>
             <div class="form-check">
               <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required>
-              <label class="form-check-label" for="debit">Debit card</label>
+              <label class="form-check-label" for="debit">Transferee</label>
             </div>
             <div class="form-check">
               <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required>
-              <label class="form-check-label" for="paypal">PayPal</label>
-            </div>
-          </div>
-
-          <div class="row gy-3">
-            <div class="col-md-6">
-              <label for="cc-name" class="form-label">Name on card</label>
-              <input type="text" class="form-control" id="cc-name" placeholder="" required>
-              <small class="text-muted">Full name as displayed on card</small>
-              <div class="invalid-feedback">
-                Name on card is required
-              </div>
-            </div>
-
-            <div class="col-md-6">
-              <label for="cc-number" class="form-label">Credit card number</label>
-              <input type="text" class="form-control" id="cc-number" placeholder="" required>
-              <div class="invalid-feedback">
-                Credit card number is required
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="cc-expiration" class="form-label">Expiration</label>
-              <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-              <div class="invalid-feedback">
-                Expiration date required
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <label for="cc-cvv" class="form-label">CVV</label>
-              <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-              <div class="invalid-feedback">
-                Security code required
-              </div>
+              <label class="form-check-label" for="paypal">Returnee</label>
             </div>
           </div>
 
@@ -278,6 +152,48 @@
       </div>
     </div>
   </main>
+</div>
+<br>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Course</th>
+      <th scope="col">Mobile No.</th> 
+      <th scope="col">Email</th> 
+      <th scope="col">Address</th>
+      <th scope="col">Student Type</th>
+    </tr>
+    
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<div class="input-group">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username with two button addons">
+  <button class="btn btn-outline-secondary" type="button">Button</button>
+  <button class="btn btn-outline-secondary" type="button">Button</button>
 </div>
 
 
